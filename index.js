c07ff1302bd85d395263366e633dcdac3d0e45cc
Kevin Angelo <kevinn.angelo@gmail.com>
2020-08-16 01:04:03 +0700

    changed port

--- index.js
+++ index.js
@@ -256,7 +256,7 @@ app.put('/module', async function (req, res){
 })
 
 // listen on port
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
